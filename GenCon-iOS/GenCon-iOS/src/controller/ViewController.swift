5b86d5983c7c2036c208dd0fccbbe4b01123125d
Taillook <jousoku@gmail.com>
2017-07-11 23:20:15 +0900

    [Add] setUp xcode project

---
+++ GenCon-iOS/GenCon-iOS/src/controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GenCon-iOS
+//
+//  Created by Taillook on 2017/07/11.
+//  Copyright © 2017年 Taillook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
