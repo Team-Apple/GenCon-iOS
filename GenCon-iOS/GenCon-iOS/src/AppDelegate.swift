c0d90dae4d9f8b642ee208758339ddc73f6f42b9
Taillook <jousoku@gmail.com>
2017-07-13 11:51:19 +0900

    [Add] add CustomUIButton

--- GenCon-iOS/GenCon-iOS/src/AppDelegate.swift
+++ GenCon-iOS/GenCon-iOS/src/AppDelegate.swift
@@ -16,8 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = ViewController()
-        window!.rootViewController = viewController
+        window!.rootViewController = HomeViewController()
         window!.makeKeyAndVisible()
         return true
     }
--- GenCon-iOS/GenCon-iOS/src/controller/ViewController.swift
+++ GenCon-iOS/GenCon-iOS/src/controller/ViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
     }
 
     override func didReceiveMemoryWarning() {
