3ca7efe0011a2cc5276edd4ceba86eb72d2f75cd
Taillook <jousoku@gmail.com>
2017-07-16 23:17:12 +0900

    [Add] navbar

--- GenCon-iOS/GenCon-iOS/src/AppDelegate.swift
+++ GenCon-iOS/GenCon-iOS/src/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window!.rootViewController = HomeVC()
+        window!.rootViewController = UINavigationController(rootViewController: HomeVC())
         window!.makeKeyAndVisible()
         setUpAppearance()
         return true
