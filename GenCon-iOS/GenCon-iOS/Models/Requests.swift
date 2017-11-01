4eba5141163320712e2e6134f578d213f25acd31
Taillook <jousoku@gmail.com>
2017-11-01 18:52:55 +0900

    [Update] エラーハンドリング

--- GenCon-iOS/GenCon-iOS/Models/Requests.swift
+++ GenCon-iOS/GenCon-iOS/Models/Requests.swift
@@ -14,13 +14,15 @@ class Requests {
     
     func fetchEvents(startDate: String, callback: @escaping ([EventObject]) -> Void) {
         Alamofire.request(baseURL + "events.json", method: .get, parameters: ["start_at_date": startDate]).validate().responseJSON{ response in
-            var data: [EventObject] = []
-            for item in (response.result.value as! [[String: Any]]) {
-                let eventObject: EventObject = Mapper<EventObject>()
-                    .map(JSONString: String(data: try! JSONSerialization.data(withJSONObject: item, options: []), encoding: .utf8)!)!
-                data.append(eventObject)
+            if response.response?.statusCode == 200 {
+                var data: [EventObject] = []
+                for item in (response.result.value as! [[String: Any]]) {
+                    let eventObject: EventObject = Mapper<EventObject>()
+                        .map(JSONString: String(data: try! JSONSerialization.data(withJSONObject: item, options: []), encoding: .utf8)!)!
+                    data.append(eventObject)
+                }
+                callback(data)
             }
-            callback(data)
         }
     }
     
