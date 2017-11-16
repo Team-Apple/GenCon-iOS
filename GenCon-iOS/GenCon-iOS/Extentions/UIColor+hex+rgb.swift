4d5d72749ebdbf0ff645ab3a6550687197f34caa
Taillook <jousoku@gmail.com>
2017-11-16 22:17:50 +0900

    [Fix] UIColor extention fot swift4

--- GenCon-iOS/GenCon-iOS/Extentions/UIColor+hex+rgb.swift
+++ GenCon-iOS/GenCon-iOS/Extentions/UIColor+hex+rgb.swift
@@ -12,33 +12,33 @@ extension UIColor {
     class var main: UIColor {
         return UIColor.hex(hexStr: "009688", alpha: 1.0)
     }
-    
+
     class var mainDark: UIColor {
         return UIColor.hex(hexStr: "00695c", alpha: 1.0)
     }
-    
+
     class var sub: UIColor {
         return UIColor.hex(hexStr: "00b8d4", alpha: 1.0)
     }
-    
+
     class func rgb(r: Int, g: Int, b: Int, alpha: CGFloat) -> UIColor{
         return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: alpha)
     }
-    
-    class func hex ( hexStr : NSString, alpha : CGFloat) -> UIColor {
+
+    class func hex(hexStr: String, alpha: CGFloat) -> UIColor {
         var hexStr = hexStr
         let alpha = alpha
-        hexStr = hexStr.replacingOccurrences(of: "#", with: "") as NSString
+        hexStr = hexStr.replacingOccurrences(of: "#", with: "")
         
-        if String(hexStr).count == 3 {
+        if hexStr.count == 3 {
             var newHexStr = ""
-            for c in String(hexStr) {
+            for c in hexStr {
                 newHexStr += "\(c)\(c)"
             }
-            hexStr = newHexStr as NSString
+            hexStr = newHexStr
         }
         
-        let scanner = Scanner(string: hexStr as String)
+        let scanner = Scanner(string: hexStr)
         var color: UInt32 = 0
         if scanner.scanHexInt32(&color) {
             let r = CGFloat((color & 0xFF0000) >> 16) / 255.0
@@ -51,4 +51,3 @@ extension UIColor {
         }
     }
 }
-
