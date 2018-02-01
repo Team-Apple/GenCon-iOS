98154d5aa5c45800c364725f4325cbf53f7b7346
Taillook <jousoku@gmail.com>
2018-02-01 23:53:32 +0900

    [ADD] error closure

--- GenCon-iOS/GenCon-iOS/Models/Requests.swift
+++ GenCon-iOS/GenCon-iOS/Models/Requests.swift
@@ -12,7 +12,7 @@ import ObjectMapper
 class Requests {
     let baseURL = "https://gencon-web.herokuapp.com/api/"
     
-    func fetchEvents(startDate: String, callback: @escaping ([EventObject]) -> Void) {
+    func fetchEvents(startDate: String, callback: @escaping ([EventObject]) -> Void, callbackError: @escaping () -> Void) {
         Alamofire.request(baseURL + "events.json", method: .get, parameters: ["start_at_date": startDate])
             .responseJSON{ response in
             if response.response?.statusCode == 200 {
@@ -21,9 +21,10 @@ class Requests {
                     let eventObject: EventObject = Mapper<EventObject>()
                         .map(JSONString: String(data: try! JSONSerialization.data(withJSONObject: item, options: []), encoding: .utf8)!)!
                     data.append(eventObject)
-                    //print(response.result.value as! [[String: Any]])
                 }
                 callback(data)
+            } else {
+                callbackError()
             }
         }
     }
